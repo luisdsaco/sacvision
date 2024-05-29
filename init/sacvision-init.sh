0ecae3a53a47f4a7cf580fc336585c16632e4f92
luisdsaco <luisdsaco@gmail.com>
2024-05-29 17:42:24 +0200

    init

--- init/sacvision-init.sh
+++ init/sacvision-init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sudo snap connect sacvision:camera :camera
-sudo snap connect sacvision:gnome-46-2404 gnome-46-2404:gnome-46-2404
+snap connect sacvision:camera :camera
+snap connect sacvision:gnome-46-2404 gnome-46-2404:gnome-46-2404
 
